3faf0d05380e4725d04c796e93b7abfc1511a179
Dani <s.leslie94@gmail.com>
2023-05-20 15:03:37 -0400

    update html pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 
-
+let pages = ['index', 'services', 'gallery', 'about', 'contact'];
 
 module.exports = {
-    entry: './src/index.js',
+    entry: pages.reduce((config, page) => {
+        config[page] = `./src/${page}.js`;
+        return config;
+      }, {}),
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js', 
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/dist/',
     },
+    optimization: {
+        splitChunks: {
+          chunks: "all",
+        },
+      },
     resolve: {
         extensions: ['.json', '.js', '.jsx'],
         modules: ['node_modules'],
@@ -31,10 +39,21 @@ module.exports = {
             },
         ],
     },
-    plugins: [new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'src/index.html',
-    })],
+    plugins: [].concat(
+        pages.map(
+          (page) =>
+            new HtmlWebpackPlugin({
+              inject: true,
+              template: `src/${page}.html`,
+              filename: `${page}.html`,
+              chunks: [page],
+            })
+        ),
+    )
+    // plugins: [new HtmlWebpackPlugin({
+    //     filename: 'index.html',
+    //     template: 'src/index.html',
+    // })],
 
 
 };
